43b5f4b31870f620f1da99c6d1f808f7ef57e024
KristinaFinck <kristina.finck83@gmail.com>
2025-04-08 15:45:18 +0300

    fix: resolve merge conflict during HW11 slider layout update + minor style adjustments

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -1,14 +1,14 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import s from './HW11.module.css'
 import s2 from '../../s1-main/App.module.css'
-import {restoreState} from '../hw06/localStorage/localStorage'
+import { restoreState } from '../hw06/localStorage/localStorage'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 
 /*
-* 1 - передать значения в оба слайдера
-* 2 - дописать типы и логику функции change
-* 3 - сделать стили в соответствии с дизайном
-* */
+ * 1 - передать значения в оба слайдера
+ * 2 - дописать типы и логику функции change
+ * 3 - сделать стили в соответствии с дизайном
+ */
 
 function HW11() {
     // for autotests // не менять // можно подсунуть в локалСторэдж нужные числа, чтоб увидеть как они отображаются
@@ -16,13 +16,12 @@ function HW11() {
     const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
 
     const change = (event: Event, value: number | number[]) => {
-        // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
         if (Array.isArray(value)) {
             // это двойной слайдер — обновляем оба значения
-            setValue1(value [0])
+            setValue1(value[0])
             setValue2(value[1])
         } else {
-            //  это одиночный слайдер
+            // это одиночный слайдер
             setValue1(value)
         }
     }
@@ -37,7 +36,6 @@ function HW11() {
                         <span id={'hw11-value'} className={s.number}>{value1}</span>
                         <SuperRange
                             id={'hw11-single-slider'}
-                            // сделать так чтоб value1 изменялось // пишет студент +
                             value={value1}
                             onChange={change}
                         />
@@ -48,7 +46,6 @@ function HW11() {
                         <SuperRange
                             id={'hw11-double-slider'}
                             value={[value1, value2]}
-                            // сделать так чтоб value1/2 изменялось // пишет студент +
                             onChange={change}
                         />
                         <span id={'hw11-value-2'} className={s.number}>{value2}</span>
