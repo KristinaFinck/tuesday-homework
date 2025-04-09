d1069a08ef3a1690d4d6299e0732e6dfe6738b24
KristinaFinck <kristina.finck83@gmail.com>
2025-04-09 23:36:05 +0300

    feat: add themeId to Redux and connect SuperSelect for dynamic theme switching

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -2,13 +2,22 @@ const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state = initState, action: changeThemeIdActionType ): InitStateType  => { // fix any +
     switch (action.type) {
-        // дописать
+        // дописать +
+        case 'SET_THEME_ID':
+            return  {
+                ...state, themeId: action.id
+            }
 
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any +
+
+type changeThemeIdActionType = ReturnType<typeof changeThemeId>
+type InitStateType = {
+    themeId: number
+}
